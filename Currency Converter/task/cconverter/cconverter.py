fcbc752d3979343f1a7e5b51c89dbc1c3ef6ab8d
Alex <rubashanov2001@gmail.com>
2023-07-25 17:03:30 +0300

    1.2
    - A new UI is here! :)
    - Extended system messages dict and main function
    - Separated functions: new get_json, get_currency, and get_amount functions.

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -2,24 +2,36 @@ import requests
 
 currency_dict = {}
 
-message_dict = {
-    'base_currency': 'Please enter the base currency: ',
-    'target_currency': 'Please enter the target currency: ',
-    'amount': 'Please enter the amount: ',
-    'checking_cache': 'Checking the cache...',
-    'not_in_cache': 'Sorry, but it is not in the cache!',
-    'in_cache': 'Oh! It is in the cache!',
-    'error_api': 'There was an error contacting the Exchange Rate API',
-    'not_understand': 'Sorry, I did not understand that.',
+sys_message_dict = {
+    'input_base_currency': 'Please enter the base currency: ',
+    'input_target_currency': 'Please enter the target currency: ',
+    'input_amount': 'Please enter the amount: ',
+    'cache_check': 'Checking the cache...',
+    'cache_miss': 'Sorry, but it is not in the cache!',
+    'cache_hit': 'Oh! It is in the cache!',
+    'api_error': 'There was an error contacting the Exchange Rate API',
+    'rate_error': 'Sorry, couldn\'t retrieve exchange rate.',
+    'invalid_input': 'Sorry, I did not understand that.',
+    'invalid_option': 'Invalid option, please choose a valid option.',
+    'conversion_conf': 'Please set the base currency, target currency, and amount first.',
+    'exit': 'Exiting ATM. Good bye!'
 }
 
 
+def get_json(base_currency):
+    r = requests.get(f'https://www.floatrates.com/daily/{base_currency}.json').json()
+    if not r:
+        print(f'Invalid currency: {base_currency}')
+        return None
+    return r
+
+
 def get_exchange_rate(base_currency, target_currency):
-    print(message_dict['checking_cache'])
+    print(sys_message_dict['cache_check'])
     if target_currency not in currency_dict:
-        print(message_dict['not_in_cache'])
+        print(sys_message_dict['cache_miss'])
         try:
-            r = requests.get(f'https://www.floatrates.com/daily/{base_currency}.json').json()
+            r = get_json(base_currency)
             rate = r.get(target_currency, {}).get('rate')
             if rate:
                 currency_dict[target_currency] = rate
@@ -27,10 +39,10 @@ def get_exchange_rate(base_currency, target_currency):
                 print(f"Could not get rate for {target_currency}")
                 return None
         except requests.RequestException:
-            print(message_dict['error_api'])
+            print(sys_message_dict['api_error'])
             return None
     else:
-        print(message_dict['in_cache'])
+        print(sys_message_dict['cache_hit'])
     return currency_dict[target_currency]
 
 
@@ -38,15 +50,19 @@ def calculate_changed_amount(amount, rate):
     return round(amount * rate, 2)
 
 
-def get_input(message, type_=None, min_=None, max_=None):
+def get_currency(message):
+    user_input = input(message).lower()
+    return user_input
+
+
+def get_amount(message, min_=None, max_=None):
     while True:
         user_input = input(message)
-        if type_ is not None:
-            try:
-                user_input = type_(user_input)
-            except ValueError:
-                print(message_dict['not_understand'])
-                continue
+        try:
+            user_input = int(user_input)
+        except ValueError:
+            print(sys_message_dict['invalid_input'])
+            continue
         if min_ is not None and user_input < min_:
             print(f'Sorry, the value cannot be less than {min_}.')
         elif max_ is not None and user_input > max_:
@@ -55,22 +71,51 @@ def get_input(message, type_=None, min_=None, max_=None):
             return user_input
 
 
+def get_ui():
+    ui_message_dict = {
+        'select_opt': '\nPlease select an option:',
+        'set_base_currency': '1: Set base currency',
+        'set_target_currency': '2: Set target currency',
+        'set_amount': '3: Set amount',
+        'convert': '4: Convert amount',
+        'exit': '5. Exit'
+    }
+
+    for x in ui_message_dict:
+        print(ui_message_dict[x])
+
+
 def main():
-    base_currency = get_input(message_dict['base_currency'], type_=str).lower()
-    target_currency = get_input(message_dict['target_currency'], type_=str).lower()
-    amount = get_input(message_dict['amount'], type_=int)
+    base_currency = None
+    target_currency = None
+    amount = None
 
     while True:
-        rate = get_exchange_rate(base_currency, target_currency)
-        if rate is None:
-            break
-        converted_amount = calculate_changed_amount(amount, rate)
-        print(f'You received {converted_amount} {target_currency.upper()}')
+        get_ui()
+        option = input("> ")
 
-        target_currency = get_input(message_dict['target_currency'], type_=str).lower()
-        if not target_currency:
+        if option == '1':
+            base_currency = get_currency(sys_message_dict['input_base_currency'])
+        elif option == '2':
+            target_currency = get_currency(sys_message_dict['input_target_currency'])
+        elif option == '3':
+            amount = get_amount(sys_message_dict['input_amount'])
+        elif option == '4':
+            if base_currency and target_currency and amount:
+                rate = get_exchange_rate(base_currency, target_currency)
+                if rate is None:
+                    print(sys_message_dict['rate_error'])
+                    continue
+                converted_amount = calculate_changed_amount(amount, rate)
+                print(f'You received {converted_amount} {target_currency.upper()}')
+            else:
+                print(sys_message_dict['conversion_conf'])
+        elif option == '5':
+            print(sys_message_dict['exit'])
             break
-        amount = get_input(message_dict['amount'], type_=int)
+        else:
+            print(sys_message_dict['invalid_option'])
+            get_ui()
 
 
 if __name__ == "__main__":
